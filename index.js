9531e3b440b18a90727070e08eafbe91f1c7f137
Aman <Palaman2626@gmail.com>
2023-08-07 16:59:28 +0530

    added eslint

--- index.js
+++ index.js
@@ -1,95 +1,93 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const morgan = require('morgan')
+require('dotenv').config(); //for loading the variables declared in .env file
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const morgan = require('morgan');
+const Note = require('./models/note');
 
+//the middlewares
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
+	response.status(404).send({ error: 'unknown endpoint' });
+};
+const errorHandler = (error, request, response, next) => {
+	console.error(error.message);
 
-app.use(cors())
-app.use(morgan('dev'))
-app.use(express.json())
-app.use(express.static('build'))
+	if (error.name === 'CastError') {
+		return response.status(400).send({ error:'malformatted id' });
+	}else if(error.name === 'ValidationError')
+	{
+		return response.status(400).json({error: error.message});
+	}
 
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    important: true
-  },
-  {
-    id: 2,
-    content: "Browser can execute only JavaScript",
-    important: false
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
-  }
-]
+	next(error);
+};
 
-
-app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
-})
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.static('build'));
 
 app.get('/api/notes', (req, res) => {
-  res.json(notes)
-})
+	Note.find({}).then((notes) => res.json(notes));
+});
 
-const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => n.id))
-    : 0
-  return maxId + 1
-}
+app.post('/api/notes', (request, response,next) => {
+	const body = request.body;
 
-app.post('/api/notes', (request, response) => {
-  const body = request.body
+	const note = new Note({
+		content: body.content,
+		important: body.important || false,
+	});
 
-  if (!body.content) {
-    return response.status(400).json({ 
-      error: 'content missing' 
-    })
-  }
+	note.save()
+		.then((savedNote) => {
+			response.json(savedNote);
+		})
+		.catch((error) => next(error));
+});
 
-  const note = {
-    content: body.content,
-    important: body.important || false,
-    date: new Date(),
-    id: generateId(),
-  }
+app.get('/api/notes/:id', (request, response,next) => {
+	Note.findById(request.params.id)
+		.then((note) => {
+			if(note)
+			{
+				response.json(note);
+			}else{
+				response.status(404).end();
+			}
+		})
+		.catch((err) => {
+			next(err);
+		});
 
-  notes = notes.concat(note)
+});
 
-  response.json(note)
-})
+app.delete('/api/notes/:id', (request, response,next) => {
+	Note.findByIdAndRemove(request.params.id)
+		.then(() => {
+			response.status(204).end();
+		})
+		.catch(error => next(error));
+});
 
-app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const note = notes.find(note => note.id === id)
+//when we perform the put requests, the validation are not by default executed so we have to do this
+//this can be easily fixed by the code below
+app.put('/api/notes/:id', (request, response, next) => {
+	const { content,important } = request.body;
 
-  if (note) {
-    response.json(note)
-  } else {
-    response.status(404).end()
-  }
+	Note.findByIdAndUpdate(request.params.id, { content,important }, { new: true, runValidators:true, context:'query' })
+		.then(updatedNote => {
+			response.json(updatedNote);
+		})
+		.catch(error => next(error));
+});
 
-  response.json(note)
-})
+app.use(unknownEndpoint); // this has to be the second last middleware.
+app.use(errorHandler); // this has to be the last loaded middleware.
 
-app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
 
-  response.status(204).end()
-})
-
-app.use(unknownEndpoint)
-
-const PORT = process.env.PORT || 3001
+// eslint-disable-next-line no-undef
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+	console.log(`Server running on port ${PORT}`);
+});
