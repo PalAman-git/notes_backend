19036f12cff62cbefa2a832ac291452ca1cdce00
Aman <Palaman2626@gmail.com>
2023-08-13 17:06:22 +0530

    added the test for all the notes routes

--- models/note.js
+++ models/note.js
@@ -1,20 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.set('strictQuery', false);
-
-
-// eslint-disable-next-line no-undef
-const url = process.env.MONGODB_URI;
-
-mongoose.connect(url)
-
-	.then(() => {
-		console.log('connected to MongoDB');
-	})
-	.catch((error) => {
-		console.log('error connecting to MongoDB:', error.message);
-	});
-
 const noteSchema = new mongoose.Schema({
 	content:{
 		type: String ,
